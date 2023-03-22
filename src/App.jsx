cccef4a2c8244f3c9c017863bdcaf4d110680cb2
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-03-22 23:18:05 +0100

    container

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="">
-      <div>
+      <div className="container">
         <h1 className="enter">Enter Temperature</h1>
         <div className="">
-          <h2>Degree</h2>
+          <h2 className="degree">Degree</h2>
           <span>0</span>
           <sup>o</sup>
           <span>C</span>
@@ -15,7 +15,7 @@ function App() {
           </select>
         </div>
         <div className="">
-          <h2>Convert In</h2>
+          <h2 className="convert">Convert In</h2>
           <p>Fahrenheit(F)</p>
           <select>
             <option></option>
