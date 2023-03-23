36ff639002cf670c84c785f39b782db5a92dded3
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-03-23 11:36:23 +0100

    answer

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,31 @@ function App() {
   return (
     <div className="">
       <div className="container">
-        <h1 className="enter">Enter Temperature</h1>
-        <div className="">
-          <h2 className="degree">Degree</h2>
-          <div className="degree-box">
-            <span>0</span>
-            <div className="degree-boxx">
-              <sup>o</sup>
-              <span>C</span>
-              <select>
-                <option>F</option>
-              </select>
+        <div className="left">
+          <h1 className="enter">Enter Temperature</h1>
+          <div className="">
+            <h2 className="degree">Degree</h2>
+            <div className="degree-box">
+              <span>0</span>
+              <div className="degree-boxx">
+                <sup>o</sup>
+                <span>C</span>
+                <select>
+                  <option>F</option>
+                </select>
+              </div>
             </div>
           </div>
+          <div className="">
+            <h2 className="convert">Convert In</h2>
+            <p>Fahrenheit(F)</p>
+            <select>
+              <option></option>
+            </select>
+            <button>Convert </button>
+          </div>
         </div>
-        <div className="">
-          <h2 className="convert">Convert In</h2>
-          <p>Fahrenheit(F)</p>
-          <select>
-            <option></option>
-          </select>
-          <button>Convert </button>
-        </div>
+
         <div className="answer">
           <span>32</span>
           <sup>o</sup>
