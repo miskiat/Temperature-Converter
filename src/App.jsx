618e3c18c6276a120ca2243d3eb27d83d37e85b2
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-03-26 01:34:43 +0100

    Left

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,44 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        {/* <div className="left">
+      <div className="containerrr">
+        <div className="left">
           <h1 className="enter">Enter Temperature</h1>
           <div className="">
             <h2 className="degree">Degree</h2>
             <div className="degree-box">
               <span>0</span>
+
               <div className="degree-boxx">
                 <sup>o</sup>
                 <span>C</span>
                 <select>
-                  <option>F</option>
+                  <option></option>
                 </select>
               </div>
             </div>
           </div>
           <div className="">
             <h2 className="convert">Convert In</h2>
-            <p>Fahrenheit(F)</p>
-            <select>
-              <option></option>
-            </select>
+            <div className="convert-box">
+              <p>
+                Fahrenheit (<sup>o</sup>F)
+              </p>
+              <select>
+                <option></option>
+              </select>
+            </div>
           </div>
-          <button>Convert</button>
-        </div> */}
+          <button className="button">Convert</button>
+        </div>
 
-        {/* <div className="answer">
+        <div className="answer">
           <span>32</span>
           <div>
             <sup>o</sup>
             <sup>F</sup>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
